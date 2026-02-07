f1b85c0f08e515376cc118ff5456297d05fff594
Lorena Peñas <lorenappff@gmail.com>
2026-02-08 00:16:31 +0100

    test: new logic tests for Book

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -4,14 +4,34 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BookTest {
+    @Test
+    public void bookHasCorrectAttributesWhenCreated() {
+        Book book = new Book("Hábitos Atómicos", "James Clear", 328);
+
+        assertEquals("Hábitos Atómicos", book.getName());
+        assertEquals("James Clear", book.getAuthor());
+        assertEquals(328, book.getNumberOfPages());
+    }
 
     @Test
-    public void bookHasCorrectAttributes() {
-        Book book = new Book("Hábitos Atómicos", "James Clear", 2, 2025);
+    public void bookHasCorrectAttributesWhenAllAdded() {
+        Book book = new Book("Hábitos Atómicos", "James Clear", 2, 2025, 328);
 
         assertEquals("Hábitos Atómicos", book.getName());
         assertEquals("James Clear", book.getAuthor());
         assertEquals(2, book.getPunctuation());
-        assertEquals(2025, book.getYearLastRead());
+        assertEquals(2025, book.getYearRead());
+        assertEquals(328, book.getNumberOfPages());
+    }
+
+    @Test
+    public void bookHasCorrectAttributesWhenAddedLater() {
+        Book book = new Book("Hábitos Atómicos", "James Clear", 328);
+
+        book.setPunctuation(2);
+        book.setYearRead(2025);
+
+        assertEquals(2, book.getPunctuation());
+        assertEquals(2025, book.getYearRead());
     }
 }
