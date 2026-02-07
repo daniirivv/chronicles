bb5ed6119b36129e698d652512584da97b2c9dcc
Lorena Peñas <lorenappff@gmail.com>
2026-02-07 22:11:32 +0100

    test: add 3 new test classes

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import java.awt.print.Book;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BookTest {
+
+    @Test
+    public void whenBookHas7_returnIncorrectPuntuation() {
+
+    }
+}
