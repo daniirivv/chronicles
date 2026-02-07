048c4780377ced7d0e5151b99ee4dcb6634ac78d
Lorena Peñas <lorenappff@gmail.com>
2026-02-08 00:21:01 +0100

    test: new logic test for Movie

--- src/test/java/MovieTest.java
+++ src/test/java/MovieTest.java
@@ -1,2 +1,15 @@
+import edu.chronicles.Book;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MovieTest {
+
+    @Test
+    public void movieHasCorrectAttributesWhenCreated() {
+        Movie movie = new Book("Inception", "Christopher Nolan");
+
+        assertEquals("Inception", movie.getName());
+        assertEquals("Christopher Nolan", movie.getDirector());
+    }
 }
