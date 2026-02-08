b8e92c6b8bacc0223ffc3e1343637c566f9cf641
daniirivv <danielriveranunezdearenas@outlook.com>
2026-02-08 05:03:56 +0100

    test: add TVShowTest.java

--- src/test/java/TVShowTest.java
+++ src/test/java/TVShowTest.java
@@ -1,3 +1,24 @@
+import edu.chronicles.Book;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TVShowTest {
 
+    private static final String SHOW_TITLE = "La Casa de Papel";
+    private static final String SHOW_DIRECTOR = "Álex Pina";
+
+    private TVShow tvShow;
+
+    @BeforeEach
+    void initializeShow(){
+        tvShow = new TVShow(SHOW_TITLE, SHOW_DIRECTOR);
+    }
+
+    @Test
+    void givenTVShowEntryParams_whenCreateNewEntry_shouldCreateCorrectEntry(){
+        assertEquals(SHOW_TITLE, tvShow.getTitle());
+        assertEquals(SHOW_DIRECTOR, tvShow.getDirector());
+    }
 }
