f285f9d30c49965df1605353beeb2f9190dea189
daniirivv <danielriveranunezdearenas@outlook.com>
2026-02-08 15:50:26 +0100

    feat: implements VehicleEntry

--- src/main/java/edu/chronicles/model/entries/VehicleEntry.java
+++ src/main/java/edu/chronicles/model/entries/VehicleEntry.java
@@ -1,5 +1,95 @@
 package edu.chronicles.model.entries;
 
-public class VehicleEntry {
+import edu.chronicles.model.Dimension;
+import edu.chronicles.model.Traction;
 
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.Currency;
+
+public class VehicleEntry extends Entry{
+
+    private String brand;
+    private Year releaseYear;
+    private Currency pvp;
+    private Dimension dimension;
+    private Float horsePower;
+    private Traction traction;
+
+    public VehicleEntry(String model,
+                        Float rating,
+                        String comments,
+                        LocalDate entryDate,
+                        String brand,
+                        Year releaseYear,
+                        Currency pvp,
+                        Dimension dimension,
+                        Float horsePower,
+                        Traction traction) {
+        super(model, rating, comments, entryDate);
+        this.brand = brand;
+        this.releaseYear = releaseYear;
+        this.pvp = pvp;
+        this.dimension = dimension;
+        this.horsePower = horsePower;
+        this.traction = traction;
+    }
+
+    public VehicleEntry(String brand, LocalDate entryDate) {
+        super(brand, entryDate);
+        this.brand = null;
+        this.releaseYear = null;
+        this.pvp = null;
+        this.dimension = null;
+        this.horsePower = null;
+        this.traction = null;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Year getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Year releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public Currency getPvp() {
+        return pvp;
+    }
+
+    public void setPvp(Currency pvp) {
+        this.pvp = pvp;
+    }
+
+    public Dimension getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(Dimension dimension) {
+        this.dimension = dimension;
+    }
+
+    public Float getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(Float horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public Traction getTraction() {
+        return traction;
+    }
+
+    public void setTraction(Traction traction) {
+        this.traction = traction;
+    }
 }
