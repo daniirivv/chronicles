22dd1c3587f0de94e4d47d4485150d912a1deb23
Lorena Peñas <lorenappff@gmail.com>
2026-02-09 00:56:26 +0100

    test: add new test for page numbers

---
+++ src/test/java/entries/BookEntryTest.java
@@ -0,0 +1,25 @@
+package entries;
+
+import edu.chronicles.model.entries.BookEntry;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BookEntryTest {
+    private static final String BOOK_TITLE = "Atomic Habits";
+    private static final String BOOK_AUTHOR = "James Clear";
+
+    private BookEntry bookEntry;
+
+    @BeforeEach
+    void initializeBook(){
+        bookEntry = new BookEntry(BOOK_TITLE);
+    }
+
+    @Test
+    void rejectsInvalidPageNumbers() {
+        assertThrows(InvalidNumberOfPagesException.class, () -> bookEntry.setPages(-1));
+    }
+
+}
