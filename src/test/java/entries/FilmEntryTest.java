1dcfb483c49981c6bdf5913d073f96ae841744ee
Lorena Peñas <lorenappff@gmail.com>
2026-02-09 19:23:22 +0100

    test: add FilmEntryTest.java

---
+++ src/test/java/entries/FilmEntryTest.java
@@ -0,0 +1,14 @@
+package entries;
+
+import edu.chronicles.model.entries.FilmEntry;
+import org.junit.jupiter.api.BeforeEach;
+
+public class FilmEntryTest {
+    private static final String FILM_NAME = "La Sociedad de la Nieve";
+
+    private FilmEntry filmEntry;
+
+    @BeforeEach
+    void initializeFilm() { filmEntry = new FilmEntry(FILM_NAME); }
+
+}
