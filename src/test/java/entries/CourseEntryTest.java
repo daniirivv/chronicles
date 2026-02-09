8f7ad86704ae2d53dd7e001c4090d1b34ac06b5f
Lorena Peñas <lorenappff@gmail.com>
2026-02-09 19:10:41 +0100

    test: create CourseEntryTest.java

---
+++ src/test/java/entries/CourseEntryTest.java
@@ -0,0 +1,25 @@
+package entries;
+
+import edu.chronicles.model.Rating;
+import edu.chronicles.model.entries.CourseEntry;
+import edu.chronicles.model.exceptions.InvalidStateException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CourseEntryTest {
+
+    private static final String COURSE_NAME = "Web Scrapping";
+
+    private CourseEntry courseEntry;
+
+    @BeforeEach
+    void initializeCourse() { courseEntry = new CourseEntry(COURSE_NAME); }
+
+    @Test
+    void shouldReturnErrorWhenRatingWithoutCompletion() {
+        assertThrows(InvalidStateException.class, () -> courseEntry.rate(Rating.of(3)));
+    }
+
+}
