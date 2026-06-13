95c5989178a7b4da4644473b3ae54b857bbb89ab
daniirivv <danielriveranunezdearenas@outlook.com>
2026-06-13 22:30:39 +0200

    minor changes

--- src/main/java/edu/chronicles/App.java
+++ src/main/java/edu/chronicles/App.java
@@ -6,19 +6,15 @@ import edu.chronicles.presentation.controllers.BookEntryController;
 import edu.chronicles.presentation.views.CLI;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
-        // 1. Montamos las piezas
         BookEntryRepository repository = new BookEntryRepository(new HashMap<>());
         BookService service = new BookService(repository);
         CLI view = new CLI();
 
-        // 2. Creamos el director y le pasamos sus herramientas
         BookEntryController controller = new BookEntryController(view, service);
 
-        // 3. ¡Empieza la función!
         controller.run();
     }
 }
