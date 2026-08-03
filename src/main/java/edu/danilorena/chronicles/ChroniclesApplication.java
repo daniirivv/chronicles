b8c97157ecc34f69dc184e8aae7cf15ce4f4826e
daniirivv <danielriveranunezdearenas@outlook.com>
2026-08-03 19:23:02 +0200

    chore: add Application entrypoints

---
+++ src/main/java/edu/danilorena/chronicles/ChroniclesApplication.java
@@ -0,0 +1,13 @@
+package edu.danilorena.chronicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChroniclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChroniclesApplication.class, args);
+	}
+
+}
