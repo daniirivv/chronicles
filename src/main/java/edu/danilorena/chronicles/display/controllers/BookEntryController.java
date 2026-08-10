f35e79b97d096e6c8c80c127ebc55f31efaba36c
daniirivv <danielriveranunezdearenas@outlook.com>
2026-08-10 13:12:05 +0200

    feat: migration complete

--- src/main/java/edu/danilorena/chronicles/display/controllers/BookEntryController.java
+++ src/main/java/edu/danilorena/chronicles/display/controllers/BookEntryController.java
@@ -1,10 +1,9 @@
 package edu.danilorena.chronicles.display.controllers;
 
 import edu.danilorena.chronicles.display.dtos.BookDto;
+import edu.danilorena.chronicles.display.dtos.UpdateBookRequest;
 import edu.danilorena.chronicles.logic.exceptions.EntryNotFoundException;
 import edu.danilorena.chronicles.logic.services.BookService;
-import edu.danilorena.chronicles.display.views.CLI;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -44,35 +43,30 @@ public class BookEntryController {
                 .build());
     }
 
-    @PatchMapping()
-    private void updateBookEntryUseCase() {
+    @PatchMapping("/{bookTitle}")
+    private ResponseEntity<BookDto> updateBookEntryUseCase(@PathVariable String bookTitle, @RequestBody UpdateBookRequest patchData) {
         try{
-            String bookTitle = view.askForInput("Introduce el título de la entrada que quieres modificar: ");
-            Optional<BookDto> toModify = this.service.retrieveBookEntry(bookTitle);
-            if (toModify.isEmpty()) {
-                throw new EntryNotFoundException(bookTitle);
-            }
-            BookDto actualBookEntry = toModify.get();
-            view.printBookEntry(actualBookEntry);
-            view.showSuccess("Añade los nuevos valores a la entrada: ");
-            BookDto modifiedBookEntry = view.askForUpdatedBookData();
-            BookDto updatedEntry = service.updateBookEntry(actualBookEntry, modifiedBookEntry);
-            view.showSuccess("Entrada modificada correctamente: ");
-            view.printBookEntry(updatedEntry);
-
+            return ResponseEntity
+                    .ok(this.service.updateBookEntry(bookTitle, patchData));
         } catch (EntryNotFoundException e) {
-            view.showError(e.getMessage());
+            return ResponseEntity
+                    .notFound()
+                    .build();
         }
     }
 
     @DeleteMapping("{bookTitle}")
     private ResponseEntity<Void> deleteBookEntryUseCase(@PathVariable String bookTitle) {
         try{
+            this.service.deleteBookEntry(bookTitle);
+
             return ResponseEntity
                     .ok()
                     .build();
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
+        } catch (EntryNotFoundException e) {
+            return ResponseEntity.notFound().build();
         }
     }
 
