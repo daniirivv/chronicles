858ed6947ac78e196d64d333810a7252bd8440db
Lorena Peñas <lorenappff@gmail.com>
2026-08-10 14:44:58 +0200

    chore(config): setup proxy to connect vite with spring boot backend

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Cualquier petición que empiece por '/books', se le envía al servidor de Spring Boot
+      'books': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      }
+    }
+  }
 })
